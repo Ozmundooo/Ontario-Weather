684fd6c13ad11d948948d2f984bddf8c28ef4341
Ozmundooo <thefatwiz@gmail.com>
2017-07-30 18:10:14 -0400

    Add project files.

---
+++ JSONConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace JSONConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String city = "";
+            Console.WriteLine("Enter City: ");
+            city = Console.ReadLine();
+            var json = new WebClient().DownloadString("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+
+                city
+                +"%2C%20on%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+            JObject data = JObject.Parse(json);
+            int temp = tempGet(data);
+
+            String condition = conditionGet(data);
+            output(city, temp, condition);
+            
+           
+        }
+
+        public static int tempGet(JObject data)
+        {
+            JObject item = (JObject)data["query"]["results"]["channel"]["item"]["condition"];
+            int temp = Convert.ToInt32(item["temp"]);
+            temp = (temp - 32) * 5 / 9;
+            return temp;
+        }
+
+
+        private static string conditionGet(JObject data)
+        {
+            JObject item = (JObject)data["query"]["results"]["channel"]["item"]["condition"];
+            String condition = Convert.ToString(item["text"]);
+            return condition;
+        }
+
+        public static void output(String city ,int temp, String condition)
+        {
+            Console.WriteLine("The temp in " + city + " is " + temp + " °C");
+            Console.WriteLine("The condition in " + city + " is " + condition);
+            Console.Read();
+        }
+    }
+}
---
+++ JSONConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JSONConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JSONConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("52982c2e-6511-4dd0-95b7-17b30cb222f1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
